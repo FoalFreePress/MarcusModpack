6b8191afe48688cefddb948d98d8352dea5e60bf
Sydney <github@sweetiebelle.org>
2021-04-25 17:02:58 -0400

    Sync from Server
    * Revert World symlink

---
+++ world/delete-world.sh
@@ -0,0 +1,8 @@
+read -r -p "You are about to delete the data for this world. Are you sure? [Y/n] " response
+if [[ "$response" =~ ^([Y])$ ]]
+then
+    cd /home/minecraft/servers/MarcusModpack/world
+    rm -rv advancements/ astralsorcery/ data/ datapacks/ deaths/ DIM-1/ DIM1/ dimensions/ EnderStorage/ level.dat level.dat_old observerlib/ playerdata/ poi/ region/ session.lock stats/
+else
+    echo "Aborting"
+fi
